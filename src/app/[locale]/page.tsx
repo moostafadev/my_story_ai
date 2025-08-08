23e977640b3459afd05ab0164dc913f7548ca421
mostafasasa010 <mostafa.sasa20004@gmail.com>
2025-08-08 17:04:19 +0300

    feat: enhance HomePage component to display additional content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,5 +2,10 @@ import { getTranslations } from "next-intl/server";
 
 export default async function HomePage() {
   const t = await getTranslations("HomePage");
-  return <h1>{t("title")}</h1>;
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <p>{t("about")}</p>
+    </div>
+  );
 }
