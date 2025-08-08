e40c3be7d1b922f82e6e36228e9b2c76551572eb
mostafasasa010 <mostafa.sasa20004@gmail.com>
2025-08-08 16:26:02 +0300

    start to project!

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,54 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { routing } from "@/i18n/routing";
+import { Roboto, Noto_Sans_Arabic } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+import "@/styles/globals.css";
+
+const roboto = Roboto({
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+const noto_sans_arabic = Noto_Sans_Arabic({
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["arabic"],
+  style: ["normal"],
+  display: "swap",
+  variable: "--font-noto-sans-arabic",
+});
+
+export const metadata: Metadata = {
+  title: "My Story AI",
+};
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+      <body
+        className={cn(
+          locale === "ar" ? noto_sans_arabic.className : roboto.className
+        )}
+      >
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
